f6b284fc17cdeb6655a31b04dbe8d17c05ef69fd
sadafamininia99 <aminicomputer96@gmail.com>
2022-01-11 11:39:23 +0330

    main layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "./Header";
 import styles from "../styles/Layout.module.css";
+import Footer from "./Footer";
 
 export default function Layout({ title, keywords, description, children }) {
   return (
@@ -13,6 +14,7 @@ export default function Layout({ title, keywords, description, children }) {
 
       <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
