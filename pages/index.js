6321302562cd1bb15c60de3eab24813a9c7216a1
sadafamininia99 <aminicomputer96@gmail.com>
2022-01-17 16:32:59 +0330

    api fech

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Layout from "@/components/Layout";
+import {API_URL} from '@/config/index'
 
-export default function HomePage() {
+
+export default function HomePage({events}) {
+  console.log('reefee')
   return (
     <Layout>
-      <h1>Home ...</h1>
+      <h1>UPcoming events.</h1>
     </Layout>
   );
+}
+
+export async function getServerSideProps(){
+  const res=await fetch(`${API_URL}/api/events`)
+  const events=await res.json()
+
+// console.log(events)log
+  return {
+    props:{events}
+  }
 }
